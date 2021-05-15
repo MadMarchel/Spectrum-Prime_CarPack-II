2858f21db3a310abc8c72feb9f35da9974e0662f
MadMarchel <bothamarchel321@gmail.com>
2021-05-15 17:05:11 +0200

    SpectrimPrime_CarPack-II

---
+++ __resource.lua
@@ -0,0 +1,136 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ --a45amg
+files {
+    'data/a45amg/vehicles.meta',
+    'data/a45amg/carvariations.meta',
+    'data/a45amg/carcols.meta',
+    'data/a45amg/handling.meta',
+    'data/a45amg/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/a45amg/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/a45amg/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/a45amg/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/a45amg/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/a45amg/vehiclelayouts.meta'
+
+ --futope
+files {
+    'data/futope/vehicles.meta',
+    'data/futope/carvariations.meta',
+    'data/futope/carcols.meta',
+    'data/futope/handling.meta',
+    'data/futope/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/futope/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/futope/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/futope/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/futope/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/futope/vehiclelayouts.meta'
+
+ --bmwm8
+files {
+    'data/bmwm8/vehicles.meta',
+    'data/bmwm8/carvariations.meta',
+    'data/bmwm8/carcols.meta',
+    'data/bmwm8/handling.meta',
+    'data/bmwm8/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/bmwm8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/bmwm8/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/bmwm8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/bmwm8/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/bmwm8/vehiclelayouts.meta'
+
+ --mgroadster
+files {
+    'data/mgroadster/vehicles.meta',
+    'data/mgroadster/carvariations.meta',
+    'data/mgroadster/carcols.meta',
+    'data/mgroadster/handling.meta',
+    'data/mgroadster/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/mgroadster/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/mgroadster/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/mgroadster/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/mgroadster/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/mgroadster/vehiclelayouts.meta'
+
+ --modelataxi
+files {
+    'data/modelataxi/vehicles.meta',
+    'data/modelataxi/carvariations.meta',
+    'data/modelataxi/carcols.meta',
+    'data/modelataxi/handling.meta',
+    'data/modelataxi/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/modelataxi/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/modelataxi/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/modelataxi/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/modelataxi/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/modelataxi/vehiclelayouts.meta'
+
+ --rebels
+files {
+    'data/rebels/vehicles.meta',
+    'data/rebels/carvariations.meta',
+    'data/rebels/carcols.meta',
+    'data/rebels/handling.meta',
+    'data/rebels/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/rebels/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/rebels/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/rebels/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/rebels/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/rebels/vehiclelayouts.meta'
+
+ --sultan2c
+files {
+    'data/sultan2c/vehicles.meta',
+    'data/sultan2c/carvariations.meta',
+    'data/sultan2c/carcols.meta',
+    'data/sultan2c/handling.meta',
+    'data/sultan2c/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/sultan2c/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/sultan2c/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/sultan2c/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/sultan2c/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/sultan2c/vehiclelayouts.meta'
+
+ --sultanrsv8
+files {
+    'data/sultanrsv8/vehicles.meta',
+    'data/sultanrsv8/carvariations.meta',
+    'data/sultanrsv8/carcols.meta',
+    'data/sultanrsv8/handling.meta',
+    'data/sultanrsv8/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/sultanrsv8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/sultanrsv8/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/sultanrsv8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/sultanrsv8/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/sultanrsv8/vehiclelayouts.meta'
+
+ --ap2
+files {
+    'data/ap2/vehicles.meta',
+    'data/ap2/carvariations.meta',
+    'data/ap2/carcols.meta',
+    'data/ap2/handling.meta',
+    'data/ap2/vehiclelayouts.meta',
+}
+
+data_file 'HANDLING_FILE'            'data/ap2/handling.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/ap2/vehicles.meta'
+data_file 'CARCOLS_FILE'             'data/ap2/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE'   'data/ap2/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'     'data/ap2/vehiclelayouts.meta'
